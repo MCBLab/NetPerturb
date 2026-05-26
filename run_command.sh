ba97f0081b7b0c1b55943e26294506949f197e8f
leoshow21 <leogabri21@gmail.com>
2026-05-26 16:23:22 -0300

    feat:code dump

--- run_command.sh
+++ run_command.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ./nextflow run main.nf \
--c resources.config \
---obj /home/lgdqamorim/scratch/projeto_nextflow/data/vehicle_final_server.rds \
---column clone_annotation \
---species human \
---n_cells 3000 \
---n_cores 32 \
---target /home/lgdqamorim/scratch/projeto_nextflow/alvos.txt \
---network hdwgcna \
---outdir results \
--profile singularity -resume
+  -c resources.config \
+  --obj /home/lgdqamorim/scratch/projeto_nextflow/data/GSE212217_pre_mutR_vs_NR.rds \
+  --column final_annotation \
+  --species human \
+  --n_cells 3000 \
+  --n_cores 32 \
+  --target /home/lgdqamorim/scratch/projeto_nextflow/alvos.txt \
+  --network hdwgcna \
+  --outdir results \
+  -profile test,singularity -resume
